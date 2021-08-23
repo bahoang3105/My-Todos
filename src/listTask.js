d0bd7f0d1385f3b4305091bd93833387f585c693
bahoang3105 <bahoang3105@gmail.com>
2021-08-23 20:51:43 +0700

    su dung life cycle, localStorage, state, props...

---
+++ src/listTask.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Task from './task';
+import ReactDOMServer from 'react-dom/server';
+
+class listTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    createTask = () => {
+        var taskName = document.getElementById('createTask').value;
+        var tasks = JSON.parse(window.localStorage.getItem("tasks"));
+        tasks.push({"name": taskName, "isComplete": "false", "id": taskName});
+        this.setState({
+            data: tasks
+        });
+        window.localStorage.setItem("tasks", JSON.stringify(tasks));
+        
+    }
+
+    
+    getData() {
+        let t = [];
+        let data = JSON.parse(window.localStorage.getItem('tasks'));
+        for(let i = 0; i < data.length; i++) {
+            t.push(<Task task={data[i]} key={data[i].id}></Task>);
+        }
+        return <div id="listTask">{t}</div>
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My Todos</h1>
+                <div>
+                    <p>Create New Task</p>
+                    <p>Name: </p>
+                    <input type="text" id="createTask"></input>
+                    <button type="submit" onClick={this.createTask}>Submit</button>
+                </div>
+                <br></br>
+                { this.getData() }
+            </div>
+        );
+    }
+}
+
+export default listTask;
--- src/task.js
+++ src/task.js
@@ -1,41 +1,80 @@
 import React from "react";
+import { unmountComponentAtNode } from "react-dom/cjs/react-dom.development";
 
 class Task extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {name: props.task.name, id: props.task.id, isComplete: props.task.isComplete, textDecorationLine: 'none', display: 'inline'};
+        this.state = {name: props.task.name, id: props.task.id, isComplete: props.task.isComplete};
     }
 
     delete = () => {
+        document.getElementById('node' + this.state.id).style.display = 'none';
         this.setState({
-            isComplete: 'true',
-            display: 'none'
+            isDelete: 'true'
         });
     }
 
     complete = () => {
         this.setState({
-            textDecorationLine: 'line-through'
+            isComplete: 'true'
+        });
+    }
+
+    update = () => {
+        let newName = document.getElementById('text' + this.state.id).value;
+        document.getElementById(this.state.id).innerHTML = newName;
+        document.getElementById('text' + this.state.id).value = '';
+        this.setState({
+            name: newName
         });
     }
 
     render() {
-        var textId = 'text' + this.state.id;
+        let textId = 'text' + this.state.id;
+        let nodeId = 'node' + this.state.id;
+        let isComplete = 'none';
+        if(this.state.isComplete === 'true') {
+            isComplete = 'line-through';
+        }
         return (
-            <div style={{ display:this.state.display }}>
-                <h3 id={this.state.id} style={{ textDecorationLine: this.state.textDecorationLine }}>{this.state.name}</h3>
+            <div style={{ display:"inline" }} id={nodeId}>
+                <h3 id={this.state.id} style={{ textDecorationLine: isComplete }}>{this.state.name}</h3>
                 <button onClick={this.delete}>Delete</button>
                 <button onClick={this.complete}>Complete</button>
                 <br></br>
                 Update New Name:
                 <input type="text" id={textId}></input>
-                <button type="submit" onClick={() => {
-                    document.getElementById(this.state.id).innerHTML = document.getElementById(textId).value;
-                    document.getElementById(textId).value = '';
-                }}>Submit</button>
+                <button type="submit" onClick={this.update}>Submit</button>
             </div>
         );
+    }
+
+    componentDidMount() {
+        console.log("component Did Mount");
+    }
+
+    componentDidUpdate() {
+        let tasks = JSON.parse(window.localStorage.getItem('tasks'));
+        if(this.state.isDelete === 'true') {
+            for(let i = 0; i < tasks.length; i++) {
+                if(tasks[i].id === this.state.id) {
+                    tasks.splice(i, 1);
+                    break;
+                }
+            }
+            window.localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+        else {
+            for(let i = 0; i < tasks.length; i++) {
+                if(tasks[i].id === this.state.id) {
+                    tasks[i].name = this.state.name;
+                    tasks[i].isComplete = this.state.isComplete;
+                    break;
+                }
+            }
+        }
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 }
 
